208f466d775f248ef30062cf1517ce046898d109
blackcanary23 <46376028+blackcanary23@users.noreply.github.com>
2019-03-11 22:29:52 +0300

    Create two.h

---
+++ two.h
@@ -0,0 +1,11 @@
+#ifndef SEC_METH_H
+#define SEC_METH_H
+
+float low_price, up_price; 
+float row, column;
+
+float temp, value_a, value_b;
+float lower_price, upper_price, average_price, accuracy;
+//int r, c;
+
+#endif
