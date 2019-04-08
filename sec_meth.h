1ed83aaff119e3bab9b6fc4bb98a16e922913067
blackcanary23 <46376028+blackcanary23@users.noreply.github.com>
2019-04-09 01:39:43 +0300

    Create sec_meth.h

---
+++ sec_meth.h
@@ -0,0 +1,10 @@
+#ifndef SEC_METH_H
+#define SEC_METH_H
+
+float low_price, up_price; 
+float row, column;
+
+float temp_a, temp_b;
+float lower_price, upper_price, average_price, accuracy, price;
+
+#endif
